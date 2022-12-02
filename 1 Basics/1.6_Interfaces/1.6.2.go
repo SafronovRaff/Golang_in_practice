6633501b96ff127c770a16ff901c978f1e8f3405
Roman W10 <Safronovraff@gmail.com>
2022-12-02 16:49:20 +0300

    interfaces

--- 1 Basics/1.6_Interfaces/1.6.2.go
+++ 1 Basics/1.6_Interfaces/1.6.2.go
@@ -19,19 +19,35 @@ type weightFunc func(element) int
 type iterator interface {
 	// чтобы понять сигнатуры методов - посмотрите,
 	// как они используются в функции max() ниже
+	next() bool
+	val() element
 }
 
 // intIterator - итератор по целым числам
 // (реализует интерфейс iterator)
 type intIterator struct {
 	// поля структуры
+	num []int
+	id  int
 }
 
 // методы intIterator, которые реализуют интерфейс iterator
+func (it *intIterator) next() bool {
+	if it.id < len(it.num) {
+		return true
+	}
+	return false
+}
+
+func (it *intIterator) val() element {
+	defer func() { it.id++ }()
+	return it.num[it.id]
+}
 
 // конструктор intIterator
 func newIntIterator(src []int) *intIterator {
 	// ...
+	return &intIterator{num: src, id: 0}
 }
 
 // ┌─────────────────────────────────┐
