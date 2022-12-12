9af99236fb560d5adab90c6e3854545eb4e730fe
Roman W10 <Safronovraff@gmail.com>
2022-12-12 13:22:07 +0300

    171

--- 1 Basics/1.7_Errors/1.7.1.go
+++ 1 Basics/1.7_Errors/1.7.1.go
@@ -43,13 +43,13 @@ var errInsufficientFunds error = errors.New("insufficient funds")
 var errInvalidAmount error = errors.New("invalid transaction amount")
 
 // account представляет счет
-type account struct {
+type accountt struct {
 	balance   int
 	overdraft int
 }
 
 // deposit зачисляет деньги на счет.
-func (acc *account) deposit(amount int) error {
+func (acc *accountt) deposit(amount int) error {
 	// ...
 	if amount > 0 {
 		acc.balance = acc.balance + amount
@@ -60,7 +60,7 @@ func (acc *account) deposit(amount int) error {
 }
 
 // withdraw списывает деньги со счета.
-func (acc *account) withdraw(amount int) error {
+func (acc *accountt) withdraw(amount int) error {
 	if acc.balance+acc.overdraft < amount {
 		return errInsufficientFunds
 	} else {
@@ -75,24 +75,24 @@ func (acc *account) withdraw(amount int) error {
 // └─────────────────────────────────┘
 
 type test struct {
-	acc   account
+	acc   accountt
 	trans []int
 }
 
 var tests = map[string]test{
-	"{100 10} [10 -50 20]":   {account{100, 10}, []int{10, -50, 20}},
-	"{30 0} [-20 -10]":       {account{30, 0}, []int{-20, -10}},
-	"{30 0}, [-20 -10 -10]":  {account{30, 0}, []int{-20, -10, -10}},
-	"{30 0}, [-100]":         {account{30, 0}, []int{-100}},
-	"{0 0}, [10 20 30]":      {account{0, 0}, []int{10, 20, 30}},
-	"{0 0}, [10 -10 20 -20]": {account{0, 0}, []int{10, -10, 20, -20}},
-	"{20 10}, [-20 -10]":     {account{20, 10}, []int{-20, -10}},
-	"{20 10}, [-20 -10 -10]": {account{20, 10}, []int{-20, -10, -10}},
-	"{0 100}, [-20 -10]":     {account{0, 100}, []int{-20, -10}},
-	"{0 30}, [-20 -10]":      {account{0, 30}, []int{-20, -10}},
-	"{0 30}, [-20 -10 -10]":  {account{0, 30}, []int{-20, -10, -10}},
-	"{70 30}, [-100 100]":    {account{70, 30}, []int{-100, 100}},
-	"{100 10}, [10 0 20]":    {account{100, 10}, []int{10, 0, 20}},
+	"{100 10} [10 -50 20]":   {accountt{100, 10}, []int{10, -50, 20}},
+	"{30 0} [-20 -10]":       {accountt{30, 0}, []int{-20, -10}},
+	"{30 0}, [-20 -10 -10]":  {accountt{30, 0}, []int{-20, -10, -10}},
+	"{30 0}, [-100]":         {accountt{30, 0}, []int{-100}},
+	"{0 0}, [10 20 30]":      {accountt{0, 0}, []int{10, 20, 30}},
+	"{0 0}, [10 -10 20 -20]": {accountt{0, 0}, []int{10, -10, 20, -20}},
+	"{20 10}, [-20 -10]":     {accountt{20, 10}, []int{-20, -10}},
+	"{20 10}, [-20 -10 -10]": {accountt{20, 10}, []int{-20, -10, -10}},
+	"{0 100}, [-20 -10]":     {accountt{0, 100}, []int{-20, -10}},
+	"{0 30}, [-20 -10]":      {accountt{0, 30}, []int{-20, -10}},
+	"{0 30}, [-20 -10 -10]":  {accountt{0, 30}, []int{-20, -10, -10}},
+	"{70 30}, [-100 100]":    {accountt{70, 30}, []int{-100, 100}},
+	"{100 10}, [10 0 20]":    {accountt{100, 10}, []int{10, 0, 20}},
 }
 
 func main() {
